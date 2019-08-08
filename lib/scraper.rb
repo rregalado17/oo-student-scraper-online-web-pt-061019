4cfd5f2449b151d2a7aa355722fb59c30036d43d
Robinson Regalado <rregalado17@gmail.com>
2019-08-08 19:07:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,20 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
+
+#base_url = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
+
+#page = open(base_url)
+
+#puts page.class
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    page = Nokogiri::HTML(open(index_url))
+    students = []
     
+
   end
 
   def self.scrape_profile_page(profile_url)
@@ -12,4 +22,6 @@ class Scraper
   end
 
 end
+
+
 
